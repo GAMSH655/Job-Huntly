036ba7fa5ce643bf8b49f7e799323abb62b8ce63
BIG_DEV360 <shittuabdquadri0@gmail.com>
2025-01-10 23:26:11 -0800

    explore

--- src/Components/Explore.jsx
+++ src/Components/Explore.jsx
@@ -20,7 +20,7 @@ const Explore = () => {
     { catImg: Human , catTitle:"human resources" ,catText:"346 jobs available"},
   ]
   return (
-    <div className=' '>
+    <div className='  m-[0px] md:m-[50px] '>
        <div className="flex justify-between">
        <h3 className="text-[20px] md:text-[3rem] m-[20px] font-bold">
         Explore by <span className="text-skyBlue">categories</span>
@@ -28,22 +28,24 @@ const Explore = () => {
         <p className="text-[#4640DE] font-semibold hidden md:flex justify-center md:items-center">show all jobs <FaArrowRight/></p>
        </div>
         <div className=" flex flex-wrap md:grid md:grid-cols-4 gap-4">
-          {
-            CateGoryData.map(({catImg , catTitle , catText} , index ) =>{
-              return <div className="border-2 border-[#D6DDEB] hover:bg-[#4640DE] hover:text-white text-[#4640DE] transition-all  w-full m-4   md:block  flex justify-center items-center" key="index">
-              <img src={catImg} alt=""  className='h-[50px] w-[50px] p-[10px]'/>
-              <h3 className='font-bold text-lg capitalize p-[10px]'>{catTitle}</h3>
-              <p className="text-sm p-[10px] flex justify-beteen">{catText} <FaArrowRight className='m-[2px]'/> </p>
-           </div>
-            })
-          }
+        {
+          CateGoryData.map(({ catImg, catTitle, catText }, index) => (
+          <div 
+            className="border-2 border-[#D6DDEB] hover:bg-[#4640DE] hover:text-white text-[#4640DE] transition-all w-full m-4 md:block flex justify-center items-center" 
+            key={index}
+          >
+            <img src={catImg} alt={catTitle} className="h-[50px] w-[50px] p-[10px]" />
+            <h3 className="font-bold text-lg capitalize p-[10px]">{catTitle}</h3>
+            <p className="text-sm p-[10px] flex justify-between">{catText} <FaArrowRight className="m-[2px]" /></p>
+          </div>
+  ))}
            <p className="text-[#4640DE] font-semibold flex justify-center items-center md:hidden m-[20px]">show all jobs <FaArrowRight/></p>
         </div>
          <div className=" mt-[50px] w-full " id='rectangle'>
          <div className="flex flex-wrap justify-center items-center md:justify-evenly p-[10px] md:p-[20px]">
          <div className=" p-[15px] md:p-[30px] ">
          <h3 className="text-white font-bold text-[1.5rem] md:text-[3rem]">Start posting  
-            jobs today
+             <span className=""> jobs today</span>
             </h3>
             <p className="text-white md:text-left text-center">start posting jobs only for $10</p>
             <button className='bg-white p-2 text-skyBlue hover:bg-[#181742]  w-full md:w-[180px] hover:text-white transition-all'>Sign up for free</button>
